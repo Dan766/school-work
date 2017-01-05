b0ee0b948e5401143c3c4d05e4cbc0f5d834a893
Dan766 <dfontana766@gmail.com>
2017-01-04 21:59:20 -0500

    Create AssembledPart.java

---
+++ 60-322/7 - Composite Pattern/AssembledPart.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public class AssembledPart extends MyStore{
+
+	public void includesComponent(String part, String comp) {
+		int currentItemIndex = parts.size()-1;
+		int compItemIndex = comp.charAt(1)-48; 
+		this.parts.get(currentItemIndex).leafs.
+		
+	}
+}
